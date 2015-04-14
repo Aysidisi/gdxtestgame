ebcfbe7b410fc233bc5518a21b291a62c57d6458
Aysidisi <scorpion_no1@gmx.de>
2015-04-14 23:10:39 +0200

    minor bugfixes in rotation and movement

--- gdxtestgame-core/src/com/aysidisi/gdxtestgame/GdxTestGame.java
+++ gdxtestgame-core/src/com/aysidisi/gdxtestgame/GdxTestGame.java
@@ -14,31 +14,28 @@ import com.badlogic.gdx.math.Vector3;
 public class GdxTestGame extends ApplicationAdapter
 {
 	private SpriteBatch batch;
-	
+
 	private OrthographicCamera camera;
-	
+
 	private Sprite sprite;
-
+	
 	private Texture texture;
-
+	
 	@Override
 	public void create()
 	{
-
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		this.batch = new SpriteBatch();
-		
 		this.texture = new Texture(Gdx.files.internal("spriteShip.png"));
 		this.sprite = new Sprite(this.texture);
 		this.sprite.setPosition(w / 2 - this.sprite.getWidth() / 2, h / 2 - this.sprite.getHeight()
 				/ 2);
-
 		this.batch = new SpriteBatch();
 		this.camera = new OrthographicCamera();
 		this.camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
-	
+
 	@Override
 	public void render()
 	{
@@ -47,9 +44,9 @@ public class GdxTestGame extends ApplicationAdapter
 		Vector3 mousePos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
 		this.camera.unproject(mousePos);
 		this.sprite
-		.setRotation((float) (Math.atan2(
-				mousePos.x - (this.sprite.getX() + this.sprite.getWidth() / 2),
-				-(mousePos.y - (this.sprite.getY() + this.sprite.getHeight() / 2))) * 180.0d / Math.PI) + 180f);
+				.setRotation((float) (Math.atan2(
+						mousePos.x - (this.sprite.getX() + this.sprite.getWidth() / 2),
+						-(mousePos.y - (this.sprite.getY() + this.sprite.getHeight() / 2))) * 180.0d / Math.PI) + 180f);
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A))
 		{
 			if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
@@ -75,9 +72,9 @@ public class GdxTestGame extends ApplicationAdapter
 		if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W))
 		{
 			this.sprite
-					.translateX((float) Math.cos(Math.toRadians(this.sprite.getRotation() + 90f)) * 1);
+			.translateX((float) Math.cos(Math.toRadians(this.sprite.getRotation() + 90f)) * 1);
 			this.sprite
-					.translateY((float) Math.sin(Math.toRadians(this.sprite.getRotation() + 90f)) * 1);
+			.translateY((float) Math.sin(Math.toRadians(this.sprite.getRotation() + 90f)) * 1);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S))
 		{
@@ -90,7 +87,6 @@ public class GdxTestGame extends ApplicationAdapter
 				this.sprite.translateY(-10.0f);
 			}
 		}
-
 		this.batch.begin();
 		this.sprite.draw(this.batch);
 		this.batch.end();
